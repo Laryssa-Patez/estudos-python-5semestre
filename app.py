0dda2609a221d08507ceae9867f2dd7fd0af0081
Laryssa Patez <laryssapatez.contato@gmail.com>
2025-03-29 00:57:12 -0300

    lista todas as enquetes e por id

--- app.py
+++ app.py
@@ -8,29 +8,54 @@ app = Flask(__name__)
 enquetes = [
     {
         'id': 1,
-        'Título':'Enquete Exemplo',
-        'Descrição':'Descrição Exemplo',
-        'Status':'Ativa',
-        'Categoria':'Cultura'
+        'titulo': 'Qual evento você gostaria que acontecesse na cidade?',
+        'descricao': 'Vote no evento que você gostaria de ver na cidade',
+        'opcoes': [
+            {'id': 1, 'descricao': 'Show de música ao vivo', 'votos': 0},
+            {'id': 2, 'descricao': 'Feira de artesanato', 'votos': 0},
+            {'id': 3, 'descricao': 'Campeonato de esportes', 'votos': 0}
+        ]
     },
     {
         'id': 2,
-        'Título':'Enquete Exemplo 2',
-        'Descrição':'Descrição Exemplo',
-        'Status':'Ativa',
-        'Categoria':'Cultura'
+        'titulo': 'Qual sua opinião sobre o novo projeto de parque?',
+        'descricao': 'Vote sobre o projeto do novo parque na cidade',
+        'opcoes': [
+            {'id': 1, 'descricao': 'Apoio ao projeto', 'votos': 0},
+            {'id': 2, 'descricao': 'Contra o projeto', 'votos': 0}
+        ]
     }
 ]
 
-# Criar enquete - POST
+# 1. Criar enquete - POST
 
-#Listar enquetes - GET
+# 2. Listar Enquetes - GET
 @app.route('/api/enquetes',methods=['GET'])
 def obter_enquetes():
     return jsonify(enquetes)
 
-#Apagar enquetes por ID - 
+# 3. Obter detalhes de uma enquete - GET
+@app.route('/api/enquetes/<int:id>',methods=['GET'])
+def obter_enquetes_por_id(id): #esse método recebe por parametro o id da enquete
+    for enquente in enquetes: 
+        if enquente['id'] == id: #Acessa os IDs na lista e valida se é igual ao id passado
+            return jsonify(enquente)
 
+# 4. Votar em uma opção de enquete - POST
+# 5. Resultados de uma enquete
+# 6. Visualizar opções de uma enquete
+# 7. Adicionar a opção em uma enquete
+# 8. Deletar enquete
+# 9. Deletar uma opção de uma enquete
+
+
+#@app.route('/api/enquetes/<int:id>/opcoes',methods=['GET'])
+#def opcoes_enquete(id): #esse método recebe por parametro o id da enquete
+#    nova_opcao = request.get_json()
+#    for indice,livro in enumerate(enquetes):
+#        if enquente.get('id') == id:
+#            enquetes.update(nova_opcao)
+#            return jsonify(livros[indice])
 
 #Subir aplicação na porta 5000
 app.run(port=5000,host='localhost',debug=True)
